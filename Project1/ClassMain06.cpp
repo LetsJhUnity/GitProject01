6da389637d751260134142a57e1428cd282321ed
unknown <jheven@naver.com>
2024-04-06 10:58:48 +0900

    2024-04-06

---
+++ Project1/ClassMain06.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include "Monster.h"
+
+//상속 구분 기준
+//Is A : ~는 ~이다.로 해석이 되는 경우
+//Has A : ~는 ~를 가지고 있다.로 해석이 되는 경우
+
+int main()
+{
+	return 0;
+}
---
+++ Project1/Monster.h
@@ -0,0 +1,29 @@
+#pragma once
+//헤더가 중복적으로 사용되는 것을 방지하기 위해 설계
+#ifndef _MONSTER_H_
+#define _MONSTER_H_
+
+#include "MonsterStatus.h"
+
+//전처리기 ifdef는 해당 값이 정의되어 있다면에 대한  작업을 진행할 수 있습니다.
+//전처리기 ifndef는 컴파일이 되어있지 않을 경우에 대한 내용을 컴파일 할 수 있습니다.
+//전처리기 else는 위의 ifdef나 ifndef의 조건과 반대의 경우를 설계하는 코드
+//전처리기 endif는 조건문의 마지막에 넣어주는 코드
+
+//클래스(cpp + h)
+//h : 클래스 설계
+//cpp : 클래스에서 설계한 함수에 대한 구현
+
+class Monster
+{
+private :
+	MonsterStatus m_status;
+	//Has a 상속
+	//몬스터 클래스는 몬스터 스테이터스 클래스를 가지고 있습니다.
+};
+
+#endif;
+
+//해당 코드의 처리 방식
+//1. ifndef _MONSTER_H를 읽는다.
+//2. _MONSTER_H가 정의되어 있으므로, endif 까지의 내용을 읽어낸다.
